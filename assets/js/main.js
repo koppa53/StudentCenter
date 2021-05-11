2513ed9bf408ebc23727b696b839da0bc0a8b613
koppa53 <jeffersonclemente.70@gmail.com>
2021-05-11 14:48:33 +0800

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Editorial by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 const log_url = "https://softeng.jbtabz.com/auth/logout";
 
 function rdrct() {
